48115558dbaf1afa3f0c948d36d7d928c751a096
Richard <richardchoi54@gmail.com>
2025-01-23 12:54:51 -0500

    clean and organize manageappts code

--- src/hooks/ManageAppointments/DisplayAppointments.tsx
+++ src/hooks/ManageAppointments/DisplayAppointments.tsx
@@ -1,54 +1,9 @@
-import api from "../api/api";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Appointment } from "../middleware/Interfaces";
-import axios from "axios";
-import { SetCacheID } from "../middleware/Cache";
-
-export async function handleDeleteAppointment(id: string | undefined) {
-  await api.deleteDocument(
-    import.meta.env.VITE_REACT_APP_DATABASE_ID,
-    import.meta.env.VITE_REACT_APP_COLLECTION_ID,
-    id,
-  );
-  window.location.reload();
-}
-
-async function NotifyClient(
-  email: string,
-  service: string,
-  year: string,
-  model: string,
-  make: string,
-  estimateId?: string,
-) {
-  try {
-    const formData = new URLSearchParams();
-    formData.append("email", email);
-    formData.append("service", service);
-    formData.append("carYear", year);
-    formData.append("carModel", model);
-    formData.append("carMake", make);
-
-    await api.deleteDocument(
-      import.meta.env.VITE_REACT_APP_DATABASE_ID,
-      import.meta.env.VITE_REACT_APP_COLLECTION_ID,
-      estimateId,
-    );
-
-    const data = await axios.post(
-      "https://car-app-backend-0ejb.onrender.com/sendEmail",
-      formData,
-      {},
-    );
-
-    if (data) {
-      window.location.reload();
-    }
-  } catch (err) {
-    console.error(err);
-  }
-}
+import { Appointment } from "../../middleware/Interfaces";
+import { SetCacheID } from "../../middleware/Cache";
+import { handleDeleteAppointment } from "./HandleDeleteAppointment";
+import { NotifyClient } from "./NotifyClient";
 
 export function displayAppointments(
   appointments: Appointment[],
